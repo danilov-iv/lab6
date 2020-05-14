e0b39d9241a9afd06665f38ca68bd50d6e70070d
Ilya <mu.ilya.danilov.1999@mail.ru>
2020-05-14 13:30:52 +0300

    first commit

---
+++ lab6/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Eq = new System.Windows.Forms.Button();
+            this.CE = new System.Windows.Forms.Button();
+            this.Num0 = new System.Windows.Forms.Button();
+            this.Dot = new System.Windows.Forms.Button();
+            this.Div = new System.Windows.Forms.Button();
+            this.Num1 = new System.Windows.Forms.Button();
+            this.Num2 = new System.Windows.Forms.Button();
+            this.Num3 = new System.Windows.Forms.Button();
+            this.Mult = new System.Windows.Forms.Button();
+            this.Num4 = new System.Windows.Forms.Button();
+            this.Num5 = new System.Windows.Forms.Button();
+            this.Sub = new System.Windows.Forms.Button();
+            this.Num7 = new System.Windows.Forms.Button();
+            this.Num8 = new System.Windows.Forms.Button();
+            this.Num9 = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
+            this.Num6 = new System.Windows.Forms.Button();
+            this.Display = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.Eq, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.CE, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.Num0, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.Dot, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.Div, 3, 4);
+            this.tableLayoutPanel1.Controls.Add(this.Num1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.Num2, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.Num3, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.Mult, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.Num4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.Num5, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.Sub, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.Num7, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Num8, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Num9, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Add, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Num6, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.Display, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(291, 353);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // Eq
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.Eq, 4);
+            this.Eq.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Eq.Location = new System.Drawing.Point(3, 293);
+            this.Eq.Name = "Eq";
+            this.Eq.Size = new System.Drawing.Size(285, 57);
+            this.Eq.TabIndex = 0;
+            this.Eq.Text = "=";
+            this.Eq.UseVisualStyleBackColor = true;
+            this.Eq.Click += new System.EventHandler(this.Eq_Click);
+            // 
+            // CE
+            // 
+            this.CE.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CE.Location = new System.Drawing.Point(3, 235);
+            this.CE.Name = "CE";
+            this.CE.Size = new System.Drawing.Size(66, 52);
+            this.CE.TabIndex = 1;
+            this.CE.Text = "CE";
+            this.CE.UseVisualStyleBackColor = true;
+            this.CE.Click += new System.EventHandler(this.CE_Click);
+            // 
+            // Num0
+            // 
+            this.Num0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Num0.Location = new System.Drawing.Point(75, 235);
+            this.Num0.Name = "Num0";
+            this.Num0.Size = new System.Drawing.Size(66, 52);
+            this.Num0.TabIndex = 2;
+            this.Num0.Text = "0";
+            this.Num0.UseVisualStyleBackColor = true;
+            this.Num0.Click += new System.EventHandler(this.Num0_Click);
+            // 
+            // Dot
+            // 
+            this.Dot.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Dot.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dot.Location = new System.Drawing.Point(147, 235);
+            this.Dot.Name = "Dot";
+            this.Dot.Size = new System.Drawing.Size(66, 52);
+            this.Dot.TabIndex = 3;
+            this.Dot.Text = ".";
+            this.Dot.UseVisualStyleBackColor = true;
+            this.Dot.Click += new System.EventHandler(this.Dot_Click);
+            // 
+            // Div
+            // 
+            this.Div.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Div.Location = new System.Drawing.Point(219, 235);
+            this.Div.Name = "Div";
+            this.Div.Size = new System.Drawing.Size(69, 52);
+            this.Div.TabIndex = 4;
+            this.Div.Text = "/";
+            this.Div.UseVisualStyleBackColor = true;
+            this.Div.Click += new System.EventHandler(this.Div_Click);
+            // 
+            // Num1
+            // 
+            this.Num1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Num1.Location = new System.Drawing.Point(3, 177);
+            this.Num1.Name = "Num1";
+            this.Num1.Size = new System.Drawing.Size(66, 52);
+            this.Num1.TabIndex = 5;
+            this.Num1.Text = "1";
+            this.Num1.UseVisualStyleBackColor = true;
+            this.Num1.Click += new System.EventHandler(this.Num1_Click);
+
+            // 
+            // Num2
+            // 
+            this.Num2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Num2.Location = new System.Drawing.Point(75, 177);
+            this.Num2.Name = "Num2";
+            this.Num2.Size = new System.Drawing.Size(66, 52);
+            this.Num2.TabIndex = 6;
+            this.Num2.Text = "2";
+            this.Num2.UseVisualStyleBackColor = true;
+            this.Num2.Click += new System.EventHandler(this.Num2_Click);
+            // 
+            // Num3
+            // 
+            this.Num3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Num3.Location = new System.Drawing.Point(147, 177);
+            this.Num3.Name = "Num3";
+            this.Num3.Size = new System.Drawing.Size(66, 52);
+            this.Num3.TabIndex = 7;
+            this.Num3.Text = "3";
+            this.Num3.UseVisualStyleBackColor = true;
+            this.Num3.Click += new System.EventHandler(this.Num3_Click);
+            // 
+            // Mult
+            // 
+            this.Mult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Mult.Location = new System.Drawing.Point(219, 177);
+            this.Mult.Name = "Mult";
+            this.Mult.Size = new System.Drawing.Size(69, 52);
+            this.Mult.TabIndex = 8;
+            this.Mult.Text = "*";
+            this.Mult.UseVisualStyleBackColor = true;
+            this.Mult.Click += new System.EventHandler(this.Mult_Click);
+
+            // 
+            // Num4
+            // 
+            this.Num4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Num4.Location = new System.Drawing.Point(3, 119);
+            this.Num4.Name = "Num4";
+            this.Num4.Size = new System.Drawing.Size(66, 52);
+            this.Num4.TabIndex = 9;
+            this.Num4.Text = "4";
+            this.Num4.UseVisualStyleBackColor = true;
+            this.Num4.Click += new System.EventHandler(this.Num4_Click);
+            // 
+            // Num5
+            // 
+            this.Num5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Num5.Location = new System.Drawing.Point(75, 119);
+            this.Num5.Name = "Num5";
+            this.Num5.Size = new System.Drawing.Size(66, 52);
+            this.Num5.TabIndex = 10;
+            this.Num5.Text = "5";
+            this.Num5.UseVisualStyleBackColor = true;
+            this.Num5.Click += new System.EventHandler(this.Num5_Click);
+            // 
+            // Sub
+            // 
+            this.Sub.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Sub.Location = new System.Drawing.Point(219, 119);
+            this.Sub.Name = "Sub";
+            this.Sub.Size = new System.Drawing.Size(69, 52);
+            this.Sub.TabIndex = 12;
+            this.Sub.Text = "-";
+            this.Sub.UseVisualStyleBackColor = true;
+            this.Sub.Click += new System.EventHandler(this.Sub_Click);
+            // 
+            // Num7
+            // 
+            this.Num7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Num7.Location = new System.Drawing.Point(3, 61);
+            this.Num7.Name = "Num7";
+            this.Num7.Size = new System.Drawing.Size(66, 52);
+            this.Num7.TabIndex = 13;
+            this.Num7.Text = "7";
+            this.Num7.UseVisualStyleBackColor = true;
+            this.Num7.Click += new System.EventHandler(this.Num7_Click);
+            // 
+            // Num8
+            // 
+            this.Num8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Num8.Location = new System.Drawing.Point(75, 61);
+            this.Num8.Name = "Num8";
+            this.Num8.Size = new System.Drawing.Size(66, 52);
+            this.Num8.TabIndex = 14;
+            this.Num8.Text = "8";
+            this.Num8.UseVisualStyleBackColor = true;
+            this.Num8.Click += new System.EventHandler(this.Num8_Click);
+            // 
+            // Num9
+            // 
+            this.Num9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Num9.Location = new System.Drawing.Point(147, 61);
+            this.Num9.Name = "Num9";
+            this.Num9.Size = new System.Drawing.Size(66, 52);
+            this.Num9.TabIndex = 15;
+            this.Num9.Text = "9";
+            this.Num9.UseVisualStyleBackColor = true;
+            this.Num9.Click += new System.EventHandler(this.Num9_Click);
+            // 
+            // Add
+            // 
+            this.Add.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Add.Location = new System.Drawing.Point(219, 61);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(69, 52);
+            this.Add.TabIndex = 16;
+            this.Add.Text = "+";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Num6
+            // 
+            this.Num6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Num6.Location = new System.Drawing.Point(147, 119);
+            this.Num6.Name = "Num6";
+            this.Num6.Size = new System.Drawing.Size(66, 52);
+            this.Num6.TabIndex = 11;
+            this.Num6.Text = "6";
+            this.Num6.UseVisualStyleBackColor = true;
+            this.Num6.Click += new System.EventHandler(this.Num6_Click);
+            // 
+            // Display
+            // 
+            this.Display.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.tableLayoutPanel1.SetColumnSpan(this.Display, 4);
+            this.Display.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Display.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Display.Location = new System.Drawing.Point(0, 0);
+            this.Display.Margin = new System.Windows.Forms.Padding(0);
+            this.Display.Multiline = true;
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(291, 58);
+            this.Display.TabIndex = 17;
+            this.Display.Text = "0";
+            this.Display.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 353);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(450, 600);
+            this.MinimumSize = new System.Drawing.Size(300, 400);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPreview = true;
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button Eq;
+        private System.Windows.Forms.Button CE;
+        private System.Windows.Forms.Button Num0;
+        private System.Windows.Forms.Button Dot;
+        private System.Windows.Forms.Button Div;
+        private System.Windows.Forms.Button Num1;
+        private System.Windows.Forms.Button Num2;
+        private System.Windows.Forms.Button Num3;
+        private System.Windows.Forms.Button Mult;
+        private System.Windows.Forms.Button Num4;
+        private System.Windows.Forms.Button Num5;
+        private System.Windows.Forms.Button Num6;
+        private System.Windows.Forms.Button Sub;
+        private System.Windows.Forms.Button Num7;
+        private System.Windows.Forms.Button Num8;
+        private System.Windows.Forms.Button Num9;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.TextBox Display;
+    }
+}
+
---
+++ lab6/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool dot_flag;
+        private bool operation_flag;
+        private void add_symbol(int symb)
+        {
+            if (Display.Text == "0" || Display.Text == float.PositiveInfinity.ToString())
+                Display.Text = "";
+            Display.Text += symb;
+        }
+
+        private void Num0_Click(object sender, EventArgs e)
+        {
+            add_symbol(0);
+        }
+
+        private void Num1_Click(object sender, EventArgs e)
+        {
+            add_symbol(1);
+        }
+
+        private void Num2_Click(object sender, EventArgs e)
+        {
+            add_symbol(2);
+        }
+
+        private void Num3_Click(object sender, EventArgs e)
+        {
+            add_symbol(3);
+        }
+
+        private void Num4_Click(object sender, EventArgs e)
+        {
+            add_symbol(4);
+        }
+
+        private void Num5_Click(object sender, EventArgs e)
+        {
+            add_symbol(5);
+        }
+
+        private void Num6_Click(object sender, EventArgs e)
+        {
+            add_symbol(6);
+        }
+
+        private void Num7_Click(object sender, EventArgs e)
+        {
+            add_symbol(7);
+        }
+
+        private void Num8_Click(object sender, EventArgs e)
+        {
+            add_symbol(8);
+        }
+
+        private void Num9_Click(object sender, EventArgs e)
+        {
+            add_symbol(9);
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            if (Display.Text.EndsWith(",") ||
+                Display.Text.EndsWith("+") || Display.Text.EndsWith("-") ||
+                Display.Text.EndsWith("*") || Display.Text.EndsWith("/") || Display.Text == float.PositiveInfinity.ToString()
+                )
+                return;
+            if (operation_flag)
+                Calculate();
+            dot_flag = false;
+            Display.Text += "+";
+            operation_flag = true;
+        }
+
+        private void Sub_Click(object sender, EventArgs e)
+        {
+            if (Display.Text.EndsWith(",") ||
+                Display.Text.EndsWith("+") || Display.Text.EndsWith("-") ||
+                Display.Text.EndsWith("*") || Display.Text.EndsWith("/") || Display.Text == float.PositiveInfinity.ToString()
+                )
+                return;
+            if (operation_flag)
+                Calculate();
+            dot_flag = false;
+            Display.Text += "-";
+            operation_flag = true;
+
+        }
+
+        private void Mult_Click(object sender, EventArgs e)
+        {
+            if (Display.Text.EndsWith(",") ||
+                Display.Text.EndsWith("+") || Display.Text.EndsWith("-") ||
+                Display.Text.EndsWith("*") || Display.Text.EndsWith("/") || Display.Text == float.PositiveInfinity.ToString()
+                )
+                return;
+            if (operation_flag)
+                Calculate();
+            dot_flag = false;
+            Display.Text += "*";
+            operation_flag = true;
+
+        }
+
+        private void Div_Click(object sender, EventArgs e)
+        {
+            if (Display.Text.EndsWith(",") ||
+                Display.Text.EndsWith("+") || Display.Text.EndsWith("-") ||
+                Display.Text.EndsWith("*") || Display.Text.EndsWith("/") || Display.Text == float.PositiveInfinity.ToString()
+                )
+                return;
+            if (operation_flag)
+                Calculate();
+            dot_flag = false;
+            Display.Text += "/";
+            operation_flag = true;
+
+        }
+
+        private void Dot_Click(object sender, EventArgs e)
+        {
+            if (dot_flag ||
+                Display.Text.EndsWith("+") || Display.Text.EndsWith("-") ||
+                Display.Text.EndsWith("*") || Display.Text.EndsWith("/") || Display.Text == float.PositiveInfinity.ToString()
+                )
+                return;
+            Display.Text += ",";
+            dot_flag = true;
+        }
+
+        private void Eq_Click(object sender, EventArgs e)
+        {
+            dot_flag = false;
+            operation_flag = false;
+            Calculate();
+        }
+
+        private void CE_Click(object sender, EventArgs e)
+        {
+            Display.Text = "0";
+            dot_flag = false;
+            operation_flag = false;
+        }
+
+        private void Calculate()
+        {
+            var reg = Regex.Match(Display.Text, @"(-?\d+(,\d+)?)[-+*/]");
+            if (!reg.Success)
+                return;
+            var fir = reg.Value;
+            var op = fir.Last();
+            var y = float.Parse(Display.Text.Substring(fir.Length));
+            fir = fir.Substring(0, fir.Length - 1);
+            var x = float.Parse(fir);
+            switch (op)
+            {
+                case '+':
+                    Display.Text = (x + y).ToString();
+                    break;
+                case '-':
+                    Display.Text = (x - y).ToString();
+                    break;
+                case '*':
+                    Display.Text = (x * y).ToString();
+                    break;
+                case '/':
+                    Display.Text = (x / y).ToString();
+                    break;
+            }
+            if (Display.Text.Contains('E'))
+                Display.Text = float.PositiveInfinity.ToString();
+            if (Display.Text.Contains(','))
+                dot_flag = true;
+            Clipboard.SetText(Display.Text);
+        }
+        void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= 48 && e.KeyChar <= 57)
+                add_symbol(e.KeyChar - 48);
+            if (e.KeyChar == '/')
+                Div_Click(sender, e);
+            if (e.KeyChar == '.')
+                Dot_Click(sender, e);
+            if (e.KeyChar == '*')
+                Mult_Click(sender, e);
+            if (e.KeyChar == '-')
+                Sub_Click(sender, e);
+            if (e.KeyChar == '+')
+                Add_Click(sender, e);
+            if (e.KeyChar == (char)Keys.Enter)
+                Eq_Click(sender, e);
+        }
+    }
+}
